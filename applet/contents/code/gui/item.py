441274c48d4e5097794f5974930770bb29ab7061
Thomas Coopman <thomas.coopman@gmail.com>
2009-03-03 17:51:45 +0100

    implement listwidget Signed-off-by: Thomas Coopman <thomas.coopman@gmail.com>

--- applet/contents/code/gui/item.py
+++ applet/contents/code/gui/item.py
@@ -3,3 +3,29 @@ Created on Mar 3, 2009
 
 @author: Thomas Coopman
 '''
+
+from PyQt4.QtGui import QColor, QListWidget, QListWidgetItem
+
+class ItemList(QListWidget):
+    def __init__(self, parent, im):
+        QListWidget.__init__(self)
+        self.im = im
+        self.initUi()
+        
+    def initUi(self):
+        for i in self.im:
+            text = []
+            text.append(i.name)
+            text.append(": ")
+            text.append(str(i.font.family()))
+            text.append(" ")
+            text.append(str(i.font.pointSize()))
+            text.append("pt")
+            item = QListWidgetItem("".join(text), self)
+            item.setTextColor(i.color)
+            item.setFont(i.font)
+            self.addItem(item)
+        
+
+    
+    
