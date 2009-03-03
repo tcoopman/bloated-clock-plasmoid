b551c4f8ba5d407dbac7ed99d6ff709de1a878dd
Thomas Coopman <thomas.coopman@gmail.com>
2009-03-03 19:10:28 +0100

    implemented listwidget Signed-off-by: Thomas Coopman <thomas.coopman@gmail.com>

--- applet/contents/code/gui/item.py
+++ applet/contents/code/gui/item.py
@@ -4,7 +4,11 @@ Created on Mar 3, 2009
 @author: Thomas Coopman
 '''
 
-from PyQt4.QtGui import QColor, QListWidget, QListWidgetItem
+from PyQt4.QtGui import *
+from PyQt4.QtCore import *
+from PyKDE4.kdeui import KDialog
+from PyKDE4.kdecore import i18n
+from ..itemEditorUi import Ui_ItemEditor
 
 class ItemList(QListWidget):
     def __init__(self, parent, im):
@@ -13,18 +17,89 @@ class ItemList(QListWidget):
         self.initUi()
         
     def initUi(self):
-        for i in self.im:
-            text = []
-            text.append(i.name)
-            text.append(": ")
-            text.append(str(i.font.family()))
-            text.append(" ")
-            text.append(str(i.font.pointSize()))
-            text.append("pt")
-            item = QListWidgetItem("".join(text), self)
-            item.setTextColor(i.color)
-            item.setFont(i.font)
-            self.addItem(item)
+        for item in self.im:
+            self.add(item)
+            
+    def add(self, item):
+        ilwi = ItemListWidgetItem(item, self)
+        self.addItem(ilwi)
+        
+    def showEdit(self):
+        self.lastItem = self.currentItem().item
+        itemEditor = ItemEditorDialog(self, self.lastItem)
+        self.takeItem(self.currentRow())
+        self.connect(itemEditor, SIGNAL("configAccepted()"), self._updateUi)
+        itemEditor.show()
+        
+    def delete(self):
+        del self.im[self.takeItem(self.currentRow()).item]
+        
+    def _updateUi(self):
+        self.add(self.lastItem)
+        
+class ItemListWidgetItem(QListWidgetItem):
+    def __init__(self, item, parent):
+        QListWidgetItem.__init__(self,parent)
+        self.item = item
+        self._updateUi()
+        
+    def _updateUi(self):
+        text = []
+        text.append(str(self.item.name))
+        text.append(": ")
+        text.append(str(self.item.font.family()))
+        text.append(" ")
+        text.append(str(self.item.font.pointSize()))
+        text.append("pt")
+        self.setText("".join(text))
+        self.setTextColor(self.item.color)
+        
+        fontSize = QApplication.font().pointSize()
+        labelFont = QFont(self.item.font)
+        labelFont.setPointSize(fontSize)
+        self.setFont(labelFont)
+        
+
+class ItemEditorWidget(QWidget, Ui_ItemEditor):
+    def __init__(self, parent):
+        QWidget.__init__(self,parent)
+        self.setupUi(self)
+        
+        self.connect(self.nameEdit, SIGNAL("textChanged(QString)"), self.changed)
+        self.connect(self.fontChooser, SIGNAL("fontSelected(QFont)"), self.changed)
+        self.connect(self.colorButton, SIGNAL("changed(QColor)"), self.changed)
+        
+    def changed(self):
+        self.emit(SIGNAL("changed(bool)"), True)
+       
+
+class ItemEditorDialog(KDialog):
+    def __init__(self, parent, item):
+        KDialog.__init__(self,parent)
+        self.setCaption(i18n("Item editor"))
+        self.setButtons(KDialog.ButtonCode(KDialog.Ok | KDialog.Cancel|KDialog.Apply))
+        
+        w = ItemEditorWidget(self)
+        self.setMainWidget(w)
+        self.connect(self, SIGNAL("applyClicked()"), self.configAccepted)
+        self.connect(self, SIGNAL("okClicked()"), self.configAccepted)
+        self.connect(w, SIGNAL("changed(bool)"), self, SLOT("enableButtonApply(bool)"))
+            
+        self.enableButtonApply(False);
+        
+        self.item = item
+        self.updateUi()
+        
+    def updateUi(self):
+        self.mainWidget().nameEdit.setText(self.item.name)
+        self.mainWidget().fontChooser.setFont(self.item.font)
+        self.mainWidget().colorButton.setColor(self.item.color)
+         
+    def configAccepted(self):
+        self.item.name = self.mainWidget().nameEdit.text()
+        self.item.font = self.mainWidget().fontChooser.font()
+        self.item.color = self.mainWidget().colorButton.color()
+        self.emit(SIGNAL("configAccepted()"))
         
 
     
--- applet/contents/code/item.py
+++ applet/contents/code/item.py
@@ -2,13 +2,14 @@
 from PyQt4.QtGui import *
 from PyQt4.QtCore import *
 from PyKDE4.kdeui import *
-from PyKDE4.kdeui import KIcon, KIconLoader
+from PyKDE4.kdeui import KIcon
 from PyKDE4.kdecore import i18n
 from PyKDE4.plasma import Plasma
 
 
 from itemUi import *
 from itemEditorUi import *
+from gui.item import ItemList
         
 class ItemManager():
     def __init__(self):
@@ -21,6 +22,9 @@ class ItemManager():
     def __len__(self):
         return len(self.items)
     
+    def __getitem__(self, index):
+        return self.items.values().__getitem__(index)
+    
     def getList(self):
         return self.items.values()
         
@@ -30,11 +34,14 @@ class ItemManager():
         except:
             print "Item not found"
             
+    def __delitem__(self, item):
+        del self.items[item.name]
+            
     def createItem(self, font, color):
         name = self._baseName + str(len(self) + 1)
         item = self.Item(self, name, font, color)
         self.items[name] = item
-        return item
+        return item 
     
     def update(self, oldname, newname):
         if str(newname) != oldname:
@@ -54,88 +61,62 @@ class ItemManager():
         
         @name.setter
         def name(self, name):
+            name = str(name)
             self.im.update(self.name, name)
             self._name = name
-    
-
-class ItemWidget(QWidget, Ui_Item):
-    def __init__(self, parent,item):
-        QWidget.__init__(self,parent)
-        self.item = item
-        self.setupUi(self)
-        
-        self.connect(self.editButton, SIGNAL("clicked()"), self.edit)
-        self.itemEditor = None
-        
-        self._updateUi()
-        
-    def _updateUi(self):
-        brush = QBrush(self.item.color)
-        brush.setStyle(Qt.SolidPattern);
-        palette = QApplication.palette()
-        palette.setBrush(QPalette.WindowText, brush)
-        self.itemNameLabel.setPalette(palette)
-        self.fontLabel.setPalette(palette)
-        self.fontSizeLabel.setPalette(palette)
-        self.itemNameLabel.setText(self.item.name)
-        
-        fontSize = QApplication.font().pointSize()
-        labelFont = QFont(self.item.font)
-        labelFont.setPointSize(fontSize)
-        self.fontLabel.setFont(labelFont)
-        self.fontLabel.setText(self.item.font.family())
-        self.fontSizeLabel.setText(str(self.item.font.pointSize()) + "pt")
-        
-    def edit(self):
-        if not self.itemEditor:
-            self.itemEditor = ItemEditorDialog(self, self.item)
-            self.connect(self.itemEditor, SIGNAL("configAccepted()"), self._updateUi)
-        self.itemEditor.show()
      
      
 class ItemListWidget(QWidget):
     def __init__(self, parent, itemManager):
         QWidget.__init__(self, parent)
-        self.layout = QVBoxLayout()
-        self.grid = QGridLayout()
-        self.layout.addLayout(self.grid)
-        self.setLayout(self.layout)
+        self.setLayout(QVBoxLayout(self))
         self.im = itemManager
-        self._iconLoader = KIconLoader()
-        self._iconLoader.Action
         self.updateUi()
         
     def updateUi(self):
         self.nbItems = 0
-        for item in self.im:
-            self._addItem(item)
-        self._addAddButton()
+        self.itemList = ItemList(self, self.im)
+        self.layout().addWidget(self.itemList)
+        self._addButtons()
         
-    def _addAddButton(self):
-        self._addButton = QToolButton(self)
+    def _addButtons(self):
+        self._addButton = QPushButton("add", self)
         self._addButton.setIcon(KIcon("list-add"))
         self.connect(self._addButton, SIGNAL("clicked()"), self._createItem)
+        
+        self._editButton = QPushButton("edit", self)
+        self._editButton.setIcon(KIcon("list-add"))
+        self._editButton.setEnabled(False)
+        self.connect(self._editButton, SIGNAL("clicked()"), self.itemList.showEdit)
+        
+        self._deleteButton = QPushButton("delete", self)
+        self._deleteButton.setIcon(KIcon("edit-delete"))
+        self._deleteButton.setEnabled(False)
+        self.connect(self._deleteButton, SIGNAL("clicked()"), self.itemList.delete)
+        
         hbox = QHBoxLayout()
         hbox.insertStretch(0)
         hbox.addWidget(self._addButton)
-        self.layout.addLayout(hbox)
+        hbox.addWidget(self._editButton)
+        hbox.addWidget(self._deleteButton)
+        self.layout().addLayout(hbox)
         
+        self.connect(self.itemList, SIGNAL("itemSelectionChanged()"), self._enableButtons)
         
+    def _enableButtons(self):
+        if self.itemList.currentItem():
+            self._editButton.setEnabled(True)
+            self._deleteButton.setEnabled(True)
+        else:
+            self._editButton.setEnabled(False)
+            self._deleteButton.setEnabled(False)
+            
     def _createItem(self):
         theme = Plasma.Theme.defaultTheme()
         font = theme.font(Plasma.Theme.DefaultFont)
         color = theme.color(Plasma.Theme.TextColor)
         item = self.im.createItem(font, color)
-        self._addItem(item)
-            
-    def _addItem(self, item):
-        itemWidget = ItemWidget(self, item)
-        self.grid.addWidget(itemWidget, self.nbItems, 0)
-        deleteButton = QToolButton(itemWidget)
-        deleteButton.setIcon(KIcon("edit-delete"))
-        self.connect(deleteButton, SIGNAL("clicked()"), self._deleteItem)
-        self.grid.addWidget(deleteButton, self.nbItems, 1)
-        self.nbItems += 1
+        self.itemList.add(item)
         
     def _deleteItem(self):
         pass
@@ -156,43 +137,3 @@ class XMLEditWidget(QWidget):
     def xml(self):
         return self.edit.toPlainText()
 
-class ItemEditorWidget(QWidget, Ui_ItemEditor):
-    def __init__(self, parent):
-        QWidget.__init__(self,parent)
-        self.setupUi(self)
-        
-        self.connect(self.nameEdit, SIGNAL("textChanged(QString)"), self.changed)
-        self.connect(self.fontChooser, SIGNAL("fontSelected(QFont)"), self.changed)
-        self.connect(self.colorButton, SIGNAL("changed(QColor)"), self.changed)
-        
-    def changed(self):
-        self.emit(SIGNAL("changed(bool)"), True)
-       
-
-class ItemEditorDialog(KDialog):
-    def __init__(self, parent, item):
-        KDialog.__init__(self,parent)
-        self.setCaption(i18n("Item editor"))
-        self.setButtons(KDialog.ButtonCode(KDialog.Ok | KDialog.Cancel|KDialog.Apply))
-        
-        w = ItemEditorWidget(self)
-        self.setMainWidget(w)
-        self.connect(self, SIGNAL("applyClicked()"), self.configAccepted)
-        self.connect(self, SIGNAL("okClicked()"), self.configAccepted)
-        self.connect(w, SIGNAL("changed(bool)"), self, SLOT("enableButtonApply(bool)"))
-            
-        self.enableButtonApply(False);
-        
-        self.item = item
-        self.updateUi()
-        
-    def updateUi(self):
-        self.mainWidget().nameEdit.setText(self.item.name)
-        self.mainWidget().fontChooser.setFont(self.item.font)
-        self.mainWidget().colorButton.setColor(self.item.color)
-         
-    def configAccepted(self):
-        self.item.name = self.mainWidget().nameEdit.text()
-        self.item.font = self.mainWidget().fontChooser.font()
-        self.item.color = self.mainWidget().colorButton.color()
-        self.emit(SIGNAL("configAccepted()"))
